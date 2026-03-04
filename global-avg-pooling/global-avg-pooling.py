38acf1e5376b3b9953e4a07646f1ea47ffc4bdec
vu-xuan-hung <vuxuanhung2203@gmail.com>
2026-03-04 15:02:16 +0700

    Update solution for global-avg-pooling

--- global-avg-pooling/global-avg-pooling.py
+++ global-avg-pooling/global-avg-pooling.py
@@ -6,17 +6,24 @@ def global_avg_pool(x):
     Supports (C,H,W) => (C,) and (N,C,H,W) => (N,C).
     """
     # Write code here
+    # x=np.array(x)
+    # if x.ndim==3:
+    #     gapc=np.zeros(x.shape[0])
+    #     for i in range(x.shape[0]):
+    #         gapc[i]=np.mean(x[i,:,:],axis=(0,1))# lấy i thì chỉ còn 2 axis
+    # elif x.ndim==4:
+    #     gapc=np.zeros((x.shape[0],x.shape[1]))
+    #     for i in range(x.shape[0]):
+    #         for j in range(x.shape[1]):
+    #             gapc[i][j]=np.mean(x[i,j,:,:],axis=(0,1))
+    # else: 
+    #     raise ValueError
+    # return gapc
     x=np.array(x)
     if x.ndim==3:
-        gapc=np.zeros(x.shape[0])
-        for i in range(x.shape[0]):
-            gapc[i]=np.mean(x[i,:,:],axis=(0,1))# lấy i thì chỉ còn 2 axis
+       return np.mean(x,axis=(1,2))#shape (C,)
     elif x.ndim==4:
-        gapc=np.zeros((x.shape[0],x.shape[1]))
-        for i in range(x.shape[0]):
-            for j in range(x.shape[1]):
-                gapc[i][j]=np.mean(x[i,j,:,:],axis=(0,1))
+        return np.mean(x,axis=(2,3))#giữ lại axis 0 và 1 (N, C)
     else: 
         raise ValueError
-    return gapc
-    pass
+   
