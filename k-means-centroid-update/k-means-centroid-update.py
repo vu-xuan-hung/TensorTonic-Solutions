c55db6396029c0f70860a6840263c481ceab4c07
vu-xuan-hung <vuxuanhung2203@gmail.com>
2026-03-12 21:09:28 +0700

    Update solution for k-means-centroid-update

--- k-means-centroid-update/k-means-centroid-update.py
+++ k-means-centroid-update/k-means-centroid-update.py
@@ -1,19 +1,15 @@
 import numpy as np
-
 def k_means_centroid_update(points, assignments, k):
     """
     Compute new centroids as the mean of assigned points.
     """
-
-    points = np.array(points)
-    assignments = np.array(assignments)
-
-    centroid = np.zeros((k, points.shape[1]))
-
+    # Write code here
+    points=np.array(points)
+    assignments=np.array(assignments)
+    centroids=np.zeros((k,points.shape[1]))
     for i in range(k):
-        point = points[assignments == i]
-
-        if len(point) > 0:
-            centroid[i] = np.mean(point, axis=0)
-
-    return centroid.tolist()
+        point=points[assignments==i]#Boolean indexing-> no ve True tai cac vi tri =i
+        if len(point)>0:
+            centroids[i]=np.mean(point,axis=0)
+    return centroids.tolist()
+        
