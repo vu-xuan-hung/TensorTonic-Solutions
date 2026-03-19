8b25f0ef9f4602416a8f36692058172c0d759f82
vu-xuan-hung <vuxuanhung2203@gmail.com>
2026-03-19 11:19:23 +0700

    Update solution for anchor-box-generation

--- anchor-box-generation/anchor-box-generation.py
+++ anchor-box-generation/anchor-box-generation.py
@@ -4,10 +4,10 @@ def generate_anchors(feature_size, image_size, scales, aspect_ratios):
     Generate anchor boxes for object detection.
     """
     # Write code here
-    stride=image_size/feature_size
-    num_anchor=feature_size**2*len(aspect_ratios)
-
-    anchor=[]
+    stride= image_size/feature_size
+    anchorbox=[]
+    scales=np.array(scales)
+    aspect_ratios=np.array(aspect_ratios)
     for i in range(feature_size):
         for j in range(feature_size):
             cx=(j+0.5)*stride
@@ -16,5 +16,6 @@ def generate_anchors(feature_size, image_size, scales, aspect_ratios):
                 for aspect_ratio in aspect_ratios:
                     w=scale*np.sqrt(aspect_ratio)
                     h=scale/np.sqrt(aspect_ratio)
-                    anchor.append([cx-w/2,cy-h/2,cx+w/2,cy+h/2])
-    return anchor
+                    anchorbox.append([cx-w/2,cy-h/2,cx+w/2,cy+h/2])
+    return anchorbox
+        
