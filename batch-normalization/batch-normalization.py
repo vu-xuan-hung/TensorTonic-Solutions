ceb41ebb6b33a7fd6135b71f3169723967799ebd
vu-xuan-hung <vuxuanhung2203@gmail.com>
2026-03-27 10:40:07 +0700

    Update solution for batch-normalization

--- batch-normalization/batch-normalization.py
+++ batch-normalization/batch-normalization.py
@@ -10,14 +10,21 @@ def batch_norm_forward(x, gamma, beta, eps=1e-5):
     beta = np.array(beta)
     m=len(x)
     if x.ndim==4:
-        gamma = gamma.reshape(1, -1, 1, 1)
-        beta = beta.reshape(1, -1, 1, 1)
+        gamma = gamma.reshape(1, -1, 1, 1)#-1 trong reshape-> tự động suy kích thước sao cho sl không đổi
+        beta = beta.reshape(1, -1, 1, 1)#64->(1,64,1,1)
         mu=np.mean(x,axis=(0,2,3),keepdims=True)#normalize per-channel, không phải toàn tensor
-        var=np.mean((x-mu)**2,axis=(0,2,3),keepdims=True)
+        var=np.mean((x-mu)**2,axis=(0,2,3),keepdims=True)#  (0,2,3) norm trên các W,H,N, không norm C vì C các channel 
+        """
+        Mục tiêu:axis=(0,2,3)
+        Chuẩn hóa từng channel riêng biệt
+        Tức là:
+        mỗi channel có 1 mean riêng
+        mỗi channel có 1 variance riêng
+        """
         xi_hat=(x-mu)/np.sqrt(var+eps)
         yi=gamma*xi_hat+beta
         return yi
-    mu=np.mean(x,axis=0,keepdims=True)
+    mu=np.mean(x,axis=0,keepdims=True)#lấy dọc xuống, lấy giá trị của 1 feature
     var=np.mean((x-mu)**2,axis=0,keepdims=True)
     xi_hat=(x-mu)/np.sqrt(var+eps)
     yi=gamma*xi_hat+beta
