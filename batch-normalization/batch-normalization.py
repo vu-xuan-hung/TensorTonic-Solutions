b5a9bc72e050a2645c14c14adc548a53f3a73cc9
vu-xuan-hung <vuxuanhung2203@gmail.com>
2026-03-27 10:09:37 +0700

    Update solution for batch-normalization

--- batch-normalization/batch-normalization.py
+++ batch-normalization/batch-normalization.py
@@ -3,22 +3,24 @@ import numpy as np
 def batch_norm_forward(x, gamma, beta, eps=1e-5):
     """
     Forward-only BatchNorm for (N,D) or (N,C,H,W).
-    This normalization is applied independently to each feature/channel.
     """
     # Write code here
     x=np.array(x)
-    gamma=np.array(gamma)
-    beta=np.array(beta)  
+    gamma = np.array(gamma)
+    beta = np.array(beta)
+    m=len(x)
     if x.ndim==4:
         gamma = gamma.reshape(1, -1, 1, 1)
-        beta  = beta.reshape(1, -1, 1, 1)
-        u=np.mean(x,axis=(0,2,3),keepdims=True)
-        o=np.mean(np.square(x-u),axis=(0,2,3),keepdims=True)
-        xi=(x-u)/np.sqrt(o+eps)
-        yi=gamma*xi+beta
-    else:
-        u=np.mean(x,axis=0,keepdims=True)
-        o=np.mean(np.square(x-u),axis=0,keepdims=True)
-        xi=(x-u)/np.sqrt(o+eps)
-        yi=gamma*xi+beta
+        beta = beta.reshape(1, -1, 1, 1)
+        mu=np.mean(x,axis=(0,2,3),keepdims=True)
+        var=np.mean((x-mu)**2,axis=(0,2,3),keepdims=True)
+        xi_hat=(x-mu)/np.sqrt(var+eps)
+        yi=gamma*xi_hat+beta
+        return yi
+    mu=np.mean(x,axis=0,keepdims=True)
+    var=np.mean((x-mu)**2,axis=0,keepdims=True)
+    xi_hat=(x-mu)/np.sqrt(var+eps)
+    yi=gamma*xi_hat+beta
     return yi
+    
+    pass
