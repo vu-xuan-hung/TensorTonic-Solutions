d12bfa2e3d9bc4bf61d297631784cd0bbe57dd57
vu-xuan-hung <vuxuanhung2203@gmail.com>
2026-03-27 10:10:55 +0700

    Update solution for batch-normalization

--- batch-normalization/batch-normalization.py
+++ batch-normalization/batch-normalization.py
@@ -12,7 +12,7 @@ def batch_norm_forward(x, gamma, beta, eps=1e-5):
     if x.ndim==4:
         gamma = gamma.reshape(1, -1, 1, 1)
         beta = beta.reshape(1, -1, 1, 1)
-        mu=np.mean(x,axis=(0,2,3),keepdims=True)
+        mu=np.mean(x,axis=(0,2,3),keepdims=True)#normalize per-channel, không phải toàn tensor
         var=np.mean((x-mu)**2,axis=(0,2,3),keepdims=True)
         xi_hat=(x-mu)/np.sqrt(var+eps)
         yi=gamma*xi_hat+beta
