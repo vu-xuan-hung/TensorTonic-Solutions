0ae7068969f22450a01803e0f23ff4cc4a4f7060
Vu Xuan Hung <vuxuanhung2203@gmail.com>
2026-04-08 22:54:21 +0700

    Update solution for transformers-attention

--- transformer/transformers-attention/transformers-attention.py
+++ transformer/transformers-attention/transformers-attention.py
@@ -8,7 +8,7 @@ def scaled_dot_product_attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tens
     """
     # Your code here
     dimk=K.shape[-1]
-    score=torch.matmul(Q,K.transpose(-2, -1))/math.sqrt(dimk)
+    score=torch.matmul(Q,K.transpose(-2, -1))/math.sqrt(dimk)# transpose 2 chiều cuối, không liên quan đến batch
     weights=F.softmax(score,dim=-1)
     return torch.matmul(weights,V)
     pass
